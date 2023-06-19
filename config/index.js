60028a59e8b7c479204f2d2e68357d8cecd61e55
sirojiddin1020 <rahimovsirojiddin80@gmail.com>
2023-06-19 15:36:45 +0500

    fixed

--- config/index.js
+++ config/index.js
@@ -2,27 +2,27 @@ require("dotenv").config();
 
 module.exports = {
     APP: {
-        PORT: 7905, //process.env.APP_PORT,
-        SCAN_DIR: 'models', //process.env.SCAN_DIR
+        PORT: 7905,
+        SCAN_DIR: 'models',
     },
 
     /**
      * @type {import("pg").PoolConfig} DB
      */
     DB: {
-        host: '127.0.0.1', //process.env.DB_HOST,
-        port: 5432, //process.env.DB_PORT,
-        user: 'postgres', //process.env.DB_USER,
-        password: 'postgres', //process.env.DB_PASSWORD,
-        database: 'oxrana_gps', //process.env.DB_NAME,
-        application_name: 'UzTracking v3.0' //process.env.APPLICATION_NAME,
+        host: '10.119.0.11',
+        port: 5432,
+        user: 'postgres',
+        password: 'postgres',
+        database: 'oxrana_gps',
+        application_name: 'UzTracking v3.0'
     },
     DB71: {
-        host: '10.119.0.11', //process.env.DB_HOST,
-        port: 5432, //process.env.DB_PORT,
-        user: 'postgres', //process.env.DB_USER,
-        password: 'postgres', //process.env.DB_PASSWORD,
-        database: 'oxrana_monitoring_regions', //process.env.DB_NAME,
-        application_name: 'UzTracking v3.0' //process.env.APPLICATION_NAME,
+        host: '10.119.0.11',
+        port: 5432,
+        user: 'postgres',
+        password: 'postgres',
+        database: 'oxrana_monitoring_regions',
+        application_name: 'UzTracking v3.0'
     }
 };
